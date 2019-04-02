c90fd37a968b2af9d08497ea4c8accdd6047cc6a
Jasmine <jsjohn026@gmail.com>
2019-04-02 11:58:21 -0500

    resubmit commit

--- w8d2_hw_intro_to_rails/Persons/db/migrate/20190402150311_create_persons_table.rb
+++
@@ -1,9 +0,0 @@
-class CreatePersonsTable < ActiveRecord::Migration[5.2]
-  def change
-    create_table :persons do |t|
-      t.string :name, null: false
-      t.timestamps
-      t.integer :house_id, null: false
-    end
-  end
-end
