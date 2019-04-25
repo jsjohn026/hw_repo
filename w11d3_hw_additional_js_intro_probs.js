8c4f009780a765840469c6333d45ae7e638f859f
Jasmine <jsjohn026@gmail.com>
2019-04-25 09:40:38 -0500

    initial commit hw

--- w11d3_hw_additional_js_intro_probs.js
+++ w11d3_hw_additional_js_intro_probs.js
@@ -3,11 +3,11 @@
 // function titleize(names, printCallback) {
 //   let titleized = [];
 //   names.forEach(function(name) {
-//     let capitalized = name.charAt(0).toUpperCase + name.slice(1).toLowerCase;
+//     let capitalized = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
 //     let titled = `Mx. ${capitalized} Jingleheimer Schmidt`;
 //     titleized.push(titled);
 //   });
-//   return titleized;
+//   printCallback(titleized); 
 // };
 
 function titleize(names, printCallback) {
@@ -19,7 +19,7 @@ function printCallback(titleized) {
   titleized.forEach(title => console.log(title));
 };
 
-console.log(titleize(["Mary", "Brian", "Leo"], printCallback));
+// console.log(titleize(["Mary", "Brian", "Leo"], printCallback));
 
 // Phase II: Constructors, Prototypes, and this:
 
@@ -38,6 +38,32 @@ Elephant.prototype.addTrick = function(trick) {
 };
 
 Elephant.prototype.play = function() {
-  console.log(`${this.name} is ${trick}`);
+  let trick = this.tricks[Math.floor(Math.random() * this.tricks.length)];
+  console.log(`${this.name} is ${trick}!`);
 };
 
+// Elephant {
+//   name: 'Dumbo',
+//     height: 120,
+//       tricks:
+//   ['painting pictures',
+//     'spraying water for a slip and slide',
+//     'playing the cello']
+// }
+// let dumbo = new Elephant("Dumbo", 108, ["painting pictures", "spraying water for a slip and slide"]);
+// let bey = new Elephant("Beyonce", 178, ["shaking her butt", "singing and dancing in front of millions of people"]);
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+Elephant.paradeHelper = function(elephant) {
+  console.log(`${elephant.name} is trotting by!`);
+};
+
+// Elephant.paradeHelper(micah);
+// Elephant.paradeHelper();
+herd.forEach(Elephant.paradeHelper); //passed in without invoking means it is passed in as a function. This is what forEach. 
+  // invoking it passes the return value.
